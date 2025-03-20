5cacd36801e68ebe1b9455087c3e124241be283f
Niladri_Das <135485980+NiladriDas1@users.noreply.github.com>
2025-03-20 09:06:21 +0530

    Add files via upload

---
+++ data_controller/lib_c.h
@@ -0,0 +1,14 @@
+#ifndef LIBC_H
+#define LIBC_H
+        #include<stdlib.h>
+        #include<errno.h>
+        #include<math.h>    
+        #include<stdio.h>
+        #include<stdbool.h>
+        #include<float.h>
+        #include<string.h>
+        #include<stdarg.h>
+        #include<assert.h>
+        #include<complex.h>
+
+#endif
